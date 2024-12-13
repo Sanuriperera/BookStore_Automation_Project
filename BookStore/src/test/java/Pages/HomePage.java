ca33d2753e1df00cec02ae9463299ebb49b55f9c
sanuriyp <55776117+sanuriyp@users.noreply.github.com>
2024-12-13 19:22:20 +0530

    Update to home page

--- BookStore/src/test/java/Pages/HomePage.java
+++ BookStore/src/test/java/Pages/HomePage.java
@@ -1,14 +1,19 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     public WebDriver driver;
+    public By searchBox= By.id("searchBox");
+    public By loginBtn=By.id("login");
+
 
 //    Constructor that will be automatically called as soon as the object of the class is created
     public HomePage (WebDriver driver){
 
         this.driver = driver;
     }
+
 
 }
--- BookStore/src/test/java/TestSuite/TC_Register.java
+++
@@ -1,4 +0,0 @@
-package TestSuite;
-
-public class TC_Register {
-}
